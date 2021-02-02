2fea904bc7d827b112859e2c986aed8a9bf052ec
rjv18 <77681709+rjv18@users.noreply.github.com>
2021-02-02 11:53:55 +0000

    Add files via upload

--- 52.py
+++ 52.py
@@ -20,30 +20,42 @@ apoints=df[['Val_Diff']]
 
 plt.figure(0)
 plt.subplot(2, 1, 1)
-plt.plot(xpoints/360, ypoints-1000)
-plt.ylabel("Voltage Set 1")
+plt.plot(xpoints/360, ypoints)
+ax=plt.gca()
+ax.axes.yaxis.set_ticks([]) 
+plt.ylabel("MLII")
+plt.title("ECG Larger Sample Time")
 
 plt.subplot(2, 1, 2)
-plt.plot(xpoints/360, zpoints-1000)
+plt.plot(xpoints/360, zpoints)
 plt.xlabel("Time/s")
-plt.ylabel("Voltage Set 2")
+ax=plt.gca()
+ax.axes.yaxis.set_ticks([])
+plt.ylabel("V5")
 plt.show()
 
 plt.figure(1)
 plt.subplot(2,1,1)
-plt.plot(xpoints[0:4000]/360, ypoints[0:4000]-1000)
-plt.ylabel("Voltage Set 1")
+plt.plot(xpoints[0:4000]/360, ypoints[0:4000])
+ax=plt.gca()
+ax.axes.yaxis.set_ticks([])
+plt.ylabel("MLII")
+plt.title("ECG Smaller Sample Time")
 
 plt.subplot(2, 1, 2)
-plt.plot(xpoints[0:4000]/360, zpoints[0:4000]-1000)
+plt.plot(xpoints[0:4000]/360, zpoints[0:4000])
 plt.xlabel("Time/s")
-plt.ylabel("Voltage Set 2")
+ax=plt.gca()
+ax.axes.yaxis.set_ticks([])
+plt.ylabel("V5")
 plt.show()
 
-i=0
-while i<4000-1:
-	if (ypoints[i]-1000)==180:
-		print(xpoints[i]/360)
-	i=i+1
-	
-		
+#i=0
+#while i<4000-1:
+#	if (ypoints[i])==1180:
+#		print(xpoints[i]/360)
+#	i=i+1
+
+#find the time when the smallest maximum peak is reached
+#scan through the sample to see at what number sample that value occurs
+#check whether it has occurred within 0.8 of a second so 0.8*360 samples, if it has exclude it no duplicates 
