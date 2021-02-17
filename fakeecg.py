4d64d708a6480f9e676cca1d8b69543a948d39d8
rjv18 <77681709+rjv18@users.noreply.github.com>
2021-02-17 19:54:57 +0000

    Update fakeecg.py

--- fakeecg.py
+++ fakeecg.py
@@ -2,54 +2,91 @@ import pylab
 import scipy.signal as signal
 import numpy
 import math
+import random
 
+def plotecg(x, ecg):
+    pylab.plot(x/1000, ecg)
+    pylab.xlabel('Time /s')
+    pylab.ylabel('Amplitude')
+    pylab.title('Heart ECG Template')
+    pylab.show()
+    
 def fakeecg(nosamples):
-    #"Daubechies" wavelet approximates to pqrst
-    qrs = signal.wavelets.daub(4) #gives it in 7 sample values
-    # Add the gap after the pqrst when the heart is resting. 
-    samples_rest = 63 #must be 9/10 of the signal so =9*samples qrs
-    zero_array = numpy.zeros(samples_rest, dtype=float)#creates the rest section
-    pqrst_full = numpy.concatenate([qrs,zero_array]) #combines the rest section and qrs
-
+    
     # Simulated Beats per minute rate
     bpm = 60 #for a reasonably healthy person, can be changed
     bps = bpm / 60
     
     capture_length = nosamples #time in seconds of ecg 
 
-    num_heart_beats =int(capture_length * bps)
-
-    ecg_template = numpy.tile(pqrst_full , num_heart_beats)#puts all the beats together
+    num_heart_beats =int(capture_length*bps)
     
-    samplingrate=len(qrs)+samples_rest
-    return ecg_template, samplingrate
+    #"Daubechies" wavelet approximates to pqrst
+    qrs = signal.wavelets.daub(4) #gives it in 7 sample values
+    # Add the gap after the pqrst when the heart is resting.
+    
+    i=0
+    rest_array=numpy.random.randint(45, 70, size=(num_heart_beats))
+    whole_ecg=[]
+    while i<num_heart_beats:                            
+        zero_array=numpy.zeros(rest_array[i], dtype=float)
+        pqrst=numpy.concatenate([qrs, zero_array])
+        newpqrst=signal.resample(pqrst, 1000)
+        for x in newpqrst:
+            whole_ecg.append(x)
+        n=[0, 5, 10, 15, 20, 25, 30]
+        b=numpy.random.choice(n)
+        whole_ecg=whole_ecg[:len(whole_ecg)-b]
+        i+=1
+    
+    whole_ecg=signal.resample(whole_ecg, 1000*num_heart_beats)
+    """
+    code i used previously and am not sure whether i might have to use again
+    #samples_rest = 63 #must be 9/10 of the signal so =9*samples qrs
+    #zero_array = numpy.zeros(samples_rest, dtype=float)#creates the rest section
+    #pqrst_full = numpy.concatenate([qrs,zero_array]) #combines the rest section and qrs
+    
+    #newpqrst=signal.resample(pqrst_full, 1000)
+    #xnew=numpy.linspace(0, 1, 1000, endpoint=False)
+
+    #ecg_template = numpy.tile(newpqrst , num_heart_beats)#puts all the beats together
+    """
+    xarray=numpy.linspace(0, len(whole_ecg), num=len(whole_ecg))
+    return whole_ecg, xarray
 
 def randomnoise(ecg):
-    randomnoise = numpy.random.normal(0, 0.01, len(ecg))
+    randomnoise = numpy.random.normal(0, 0.1, len(ecg))
     noisyecg = randomnoise + ecg
     return noisyecg
 
-def addmains(ecg):
-    t=numpy.arange(0, (len(ecg)/70), 1/70)
-    f=50
+def addnoise(ecg, samples, f, amp):  #f is noise frequency (hertz) 
+    
+    fs = 1000 #Sampling frequency
+    dt = 1/fs #seconds per sample  
+     
+    t=numpy.arange(0, samples, dt)
     mains=numpy.sin(2*(math.pi)*f*t)
-    mains=mains/10
-    mainsecg=ecg+mains
-    return mainsecg
+    sinwave=amp*mains
+    noised=ecg+sinwave
+    return noised
 
-def plotecg(ecg):
-    pylab.plot(ecg)
-    pylab.xlabel('Sample number')
-    pylab.ylabel('Amplitude')
-    pylab.title('Heart ECG Template')
-    pylab.show()
+def addoffset(ecg, amp):
+    delta=amp*random.random()
+    offset_ecg = [x+delta for x in ecg]
+    return offset_ecg
 
+samples=60  #time in seconds of fake ecg  
+ecg, x=fakeecg(samples)
 
-samples=200  #time in seconds of fake ecg  
-x, y=fakeecg(samples)               
-mainsecg=addmains(x)
-noisedecg=randomnoise(mainsecg)
-plotecg(x[100:1000])
-plotecg(mainsecg[100:1000])
-plotecg(noisedecg[100:1000])
+mainsecg=addnoise(ecg, samples, 50, 0.25) #adds mains noise of 50Hz
+basedriftecg=addnoise(mainsecg, samples, 0.3, 0.1) #adds baseline drift of 0.3Hz
+smallerdrift=addnoise(basedriftecg, samples, 0.01, 0.2) #adds lower frequency baseline drift of 0.01Hz
+noisedecg=randomnoise(basedriftecg)
+offset=addoffset(noisedecg, 0.5)
 
+plotecg(x, ecg)
+plotecg(x, mainsecg)
+plotecg(x, basedriftecg)
+plotecg(x, smallerdrift)
+plotecg(x, noisedecg)
+plotecg(x, offset)
