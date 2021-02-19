0db736447808a4b1129a560f7c3938f3bb26497c
cay15 <calista.yapeter@gmail.com>
2021-02-19 20:40:01 +0700

    Testing filter with noisy ECG from MITBIH

--- testFilter.py
+++ testFilter.py
@@ -3,6 +3,7 @@ import pandas as pd
 from filter import plot_data,filter
 from fakeecg import whole_fakeecg
 
+'''
 # Create noisy ecg
 artificial_ecg, xtime=whole_fakeecg(120)
 
@@ -24,3 +25,18 @@ plot_data(xVal,filtered_signal,10000,'Notch Filtered')
 # High-frequency noise removal (low-pass filter)
 filtered_signal = filter(filtered_signal,1000,100,2,'lowpass')    # removes components above 150Hz
 plot_data(xVal,filtered_signal,10000,'Lowpass Filtered')
+
+
+# Import ECG (.csv), get fs
+column_names = [    #create a table with 3 columns depending on .csv file
+    't',
+    'ecg1',
+    'ecg2',
+    ]
+'''
+
+#read the .csv file into a dataframe using pandas and skip the first 2 rows
+whole_signal = pd.read_csv('MITBIH_Noise118e00.csv', sep=',',
+                           names = column_names, skiprows = 2)
+xVal = whole_signal.t
+yVal = whole_signal.ecg1
