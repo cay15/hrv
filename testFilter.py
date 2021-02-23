fec06aeb8c921e5b28e942062f66ec96d4fe478b
cay15 <calista.yapeter@gmail.com>
2021-02-24 01:36:29 +0700

    Test filters on noisy ECG resampled to 1000Hz

--- testFilter.py
+++ testFilter.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import math
 from scipy import signal
-from filter import plot_data,filter
+from filter import plot_data,filter, denoise
+
 from fakeecg import whole_fakeecg
 
 '''
@@ -38,11 +39,11 @@ column_names = [    #create a table with 3 columns depending on .csv file
     ]
 
 #read the .csv file into a dataframe using pandas and skip the first 2 rows
-whole_signal = pd.read_csv('ecgSample.csv', sep=',',
+whole_signal = pd.read_csv('/Users/calistayapeter/Documents/python/hrv/mit-bih-database/MITBIH_Noise119e18.csv', sep=',',
                            names = column_names, skiprows = 2)
 
 #read the file header and access the sampling rate
-df=pd.read_csv('ecgSample.csv', header=[0, 1]) #takes in the csv and tells the program that the first two lines are header
+df=pd.read_csv('/Users/calistayapeter/Documents/python/hrv/mit-bih-database/MITBIH_Noise119e18.csv', header=[0, 1]) #takes in the csv and tells the program that the first two lines are header
 header=df.columns #makes a list of header titles
 samplinginterval=header[0][1] #records the sampling interval but has sec on end
 numerical_sampleinterval=samplinginterval.replace(" sec", "") #removes sec so the interval is just a number
@@ -54,19 +55,21 @@ f_samp = int(1/t_samp)
 print(f_samp)
 
 # Plot the original signal
-plot_data(whole_signal.t,whole_signal.ecg1,1000,f_samp,"ECG 1")
+plot_data(whole_signal.t,whole_signal.ecg1,8*f_samp,f_samp,"ECG 1")
 
 # Split signal into chunks
-#create a new array containing first 1025 (8 waves) samples of ecg1,ecg2 and time
+#create a new array containing first 1024 (~8 waves) samples of ecg1,ecg2 and time
 short_ecg1 = np.array(whole_signal.ecg1[0:8*f_samp])
 short_ecg2 = np.array(whole_signal.ecg2[0:8*f_samp])
 time = np.array(whole_signal.t[0:8*f_samp+1])
 chunkLength = len(short_ecg1)
+resampledLength = math.floor(1000*chunkLength/f_samp)
+print(chunkLength)
 
-#resampling signal using scipy.signal.resample
+#resampling signal to 1000Hz using scipy.signal.resample
 #note: this produces a tuple
-resampled_ecg1 = signal.resample(short_ecg1,math.floor(1000*chunkLength/f_samp), t = time)   # since there are 8 waves, need about 8*1000 samples in resampled signal
-resampled_ecg2 = signal.resample(short_ecg2,math.floor(1000*chunkLength/f_samp), t = time)
+resampled_ecg1 = signal.resample(short_ecg1,resampledLength, t = time)
+resampled_ecg2 = signal.resample(short_ecg2,resampledLength, t = time)
 
 #convert tuple to dataframe
 ecg1 = np.transpose(resampled_ecg1[0])
@@ -75,10 +78,9 @@ ecg2 = np.transpose(resampled_ecg2[0])
 
 upsampled_sig = np.stack((time, ecg1, ecg2), axis = 1)
 upsampled_sig = pd.DataFrame(upsampled_sig, columns = column_names)
-print(upsampled_sig.t)
-print(upsampled_sig.ecg1)
 
-plot_data(upsampled_sig.t,upsampled_sig.ecg1,8000,f_samp,'Resampled ECG')
+
+plot_data(upsampled_sig.t,upsampled_sig.ecg1,resampledLength,f_samp,'Resampled ECG')
 
 
 ## Filter resampled ECG
@@ -93,3 +95,7 @@ plot_data(upsampled_sig.t,filtered_signal,8000,1000,'Notch Filtered')
 # High-frequency noise removal (low-pass filter)
 filtered_signal = filter(upsampled_sig.ecg1,1000,150,2,'lowpass')    # removes components above 150Hz
 plot_data(upsampled_sig.t,filtered_signal,8000,1000,'Lowpass Filtered')
+
+# Apply all filters to denoise ECG
+filteredSig = denoise(upsampled_sig.ecg1,1000,150,0.5,50,2)
+plot_data(upsampled_sig.t,filteredSig,8000,1000,'Denoised ECG')
