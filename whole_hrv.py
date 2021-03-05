e4ef5ba8d23e239bf41588ec0c707ec417fa8bac
rjv18 <77681709+rjv18@users.noreply.github.com>
2021-03-05 17:34:16 +0000

    functioned part 1a

--- whole_hrv.py
+++ whole_hrv.py
@@ -1,5 +1,3 @@
-#same code just not sampled
-
 # Main file where individual .py files are combined and HRV analysis will be done
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,7 +8,7 @@ from scipy import signal
 from artificial_ecg_1b import whole_fakeecg
 from filter_ecg_2 import plot_data,filter, denoise
 from peak_detection_3 import mirror_ecg, diffs, get_r_peaks, get_rr, hrv
-
+from input_ecg_1a import input_ecg
   
     
 def decidetype():
@@ -18,74 +16,18 @@ def decidetype():
         d1=input("Do you want to: A) Input a raw .csv ECG file or B)Use an artificial ECG? [A/B]: ")
         if d1 in ['A', 'B']: #keeps asking if the user input isn't A or B
             break
-    if d1 == "A": 
-        filename = input("Enter your filename (including .csv): ")
-        print("Loading", filename + "...")
-        ## 1. Input File
-        #Import ECG (.csv)
-        #create a table with 3 columns depending on .csv file
-        column_names = [    #create a table with 3 columns depending on .csv file
-            't',
-            'ecg1',
-            'ecg2',
-            ]
-
-        #read the .csv file into a dataframe using pandas and skip the first 2 rows
-        whole_signal = pd.read_csv(filename, sep=',',
-                           names = column_names, skiprows = 2)
-
-        #read the file header and access the sampling rate
-        df=pd.read_csv(filename, header=[0, 1]) #takes in the csv and tells the program that the first two lines are header
-        header=df.columns #makes a list of header titles
-        samplinginterval=header[0][1] #records the sampling interval but has sec on end
-        numerical_sampleinterval=samplinginterval.replace(" sec", "") #removes sec so the interval is just a number
-        numerical_sampleinterval=numerical_sampleinterval.replace("'", "") #removes "'" so str can be converted into float
-        t_samp = float(numerical_sampleinterval)
-        x=df[["'sample interval'"]]
-
-        #get the sampling frequency of original signal
-        f_samp = int(1/t_samp)
-        # Plot the original signal
-        plot_data(whole_signal.t, whole_signal.ecg1, 5*f_samp, f_samp, "ECG 1")
-    
-        
-        while True: #choice to test simulation using artificial ECG or raw ECG
-            d2=input("Do you want to: 1) Input ECG data for 1st lead in the file or 2) The 2nd lead? [1/2]: ")
-            if d2 in ['1', '2']: #keeps asking if the user input isn't 1 or 2
-                break
-        if d2 == "1": 
-            ecgchosen=whole_signal.ecg1
-        
-        elif d2 == "2": 
-            ecgchosen=whole_signal.ecg2
-             
-        time = whole_signal.t
-        Length = len(ecgchosen)
-        resampledLength = math.floor(1000*Length/f_samp)
-
-        #resampling signal to 1000Hz using scipy.signal.resample
-        #note: this produces a tuple
-        resampled_ecg = signal.resample(ecgchosen,resampledLength, t = time)
- 
-        #convert tuple to dataframe
-        ecg1 = np.transpose(resampled_ecg[0])
-        time = np.transpose(resampled_ecg[1])
-
-        upsampled_sig = np.stack((time, ecg1), axis = 1)
-        upsampled_sig = pd.DataFrame(upsampled_sig, columns = ['t', 'ECG'])
-
+    if d1 == "A":
+        ## 1. a) Input ECG
+        ecg1, time, f_samp, resampledLength=input_ecg()
         # Plot resampled ECG
-        plot_data(upsampled_sig.t,upsampled_sig.ECG,resampledLength,f_samp,'Resampled ECG')
-        
-        
-        return ecg1, upsampled_sig.t, f_samp
+        plot_data(time,ecg1,resampledLength,f_samp,'Resampled ECG')
+        return ecg1, time, f_samp
     elif d1 == "B": 
-        ## 2. Artificial ECG
+        ## 1. b) Create Artificial ECG
         artificial_points, xtime = whole_fakeecg(120)
         sampfreq=1000
         a=np.arange(len(xtime))
         b=pd.DataFrame(data=a, columns=['Samples']) 
-        samples=b[['Samples']]
         return artificial_points, xtime, sampfreq
 
 
@@ -97,14 +39,14 @@ upsampled_sig = pd.DataFrame(upsampled_sig, columns = ['t', 'ecg'])
 plot_data(upsampled_sig.t,upsampled_sig.ecg, len(upsampled_sig.t),f_samp,'Normal ECG')
 
 #
-## 3. FILTERING
+## 2. FILTERING
 # Apply all filters to denoise ECG
 filteredSig = denoise(upsampled_sig.ecg,1000,100,0.5,50,2)
 plot_data(upsampled_sig.t,filteredSig, len(upsampled_sig.t),f_samp,'Denoised ECG')
 
 # consider: anomalous/atopic beats
 
-## 4. R PEAK DETECTION
+## 3. R PEAK DETECTION
 # Mirror negative R peaks if present
 mirroredSig=mirror_ecg(filteredSig)
 # DEBUG
@@ -118,7 +60,7 @@ plt.plot(peaks,mirroredSig.iloc[peaks],"x")
 plt.title('Local peaks')
 plt.show()
 
-## 5. RR INTERVALS
+## 4. RR INTERVALS
 # edit w_t and a_t based on condition being analysed
 r_peaks=get_r_peaks(peaks,mirroredSig,0.6,3)
 # DEBUG
